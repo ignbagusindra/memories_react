3c3609dd7a39331bf9cc7ead3cbc8769fa7b97bd
I G N Bagus Indra <ignbagusindra.82@gmail.com>
2023-08-08 09:33:24 +0800

    Update index.js
    mongoDB credentials

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://bagusIndra:bagusIndra123@cluster0.x1b5ajx.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = ''; // Add yours mongoDB's  
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
